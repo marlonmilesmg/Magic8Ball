2cf3edee7c991ecd33824a1eada881b8d135868b
Marlon Magonjo <Seven@1Seven@2>
2020-02-08 14:18:42 +0200

    Magic 8 Ball Game

---
+++ app/src/main/java/www/technoextreme/co/zw/magic8ball/MainActivity.java
@@ -0,0 +1,44 @@
+package www.technoextreme.co.zw.magic8ball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay = (ImageView) findViewById(R.id.image_EightBall);
+
+        final int[] ballArray = new int[]{
+          R.drawable.ball1,
+          R.drawable.ball2,
+          R.drawable.ball3,
+          R.drawable.ball4,
+          R.drawable.ball5
+        };
+
+        Button myButton = (Button) findViewById(R.id.askButton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(5);
+
+             //   int immageResourceId = ballArray[number];
+                ballDisplay.setImageResource(ballArray[number]);
+            }
+        });
+
+
+    }
+}
